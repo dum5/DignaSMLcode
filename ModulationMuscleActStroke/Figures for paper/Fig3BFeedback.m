95caa00e9ec4794d9400829a1fe5ec066d0c1988
dignadekam <digna.dekam@gmail.com>
2018-05-21 17:51:18 -0400

    Reorganize Figure 3

--- ModulationMuscleActStroke/Figures for paper/Fig3BFeedback.m
+++ ModulationMuscleActStroke/Figures for paper/Fig3BFeedback.m
@@ -29,11 +29,11 @@ ax3 = axes('Position',[0.22   0.1   0.7 0.33],'FontSize',12);%Correlation with F
 
 hold(ax2)
 bar(ax2,1,nanmean(TControl.BM),'BarWidth',0.3,'FaceColor',[0.4 0.7 0.7]);
-errorbar(ax2,1.05,nanmean(TControl.BM),nanstd(TControl.BM)./sqrt(15),'LineWidth',2,'Color','k')
+errorbar(ax2,1,nanmean(TControl.BM),nanstd(TControl.BM)./sqrt(15),'LineWidth',2,'Color','k')
 bar(ax2,2,nanmean(TStroke.BM),'BarWidth',0.3,'FaceColor',[0.9 0.5 0.9]);
-errorbar(ax2,2.05,nanmean(TStroke.BM),nanstd(TStroke.BM)./sqrt(15),'LineWidth',2,'Color','k')
-plot(ax2,0.95,TControl.BM,'.k','MarkerSize',7)
-plot(ax2,1.95,TStroke.BM,'.k','MarkerSize',7)
+errorbar(ax2,2,nanmean(TStroke.BM),nanstd(TStroke.BM)./sqrt(15),'LineWidth',2,'Color','k')
+%plot(ax2,0.95,TControl.BM,'.k','MarkerSize',7)
+%plot(ax2,1.95,TStroke.BM,'.k','MarkerSize',7)
 set(ax2,'XLim',[0.5 2.5],'XTick',[1 2],'XTickLabel',{'CONTROL','STROKE'},...
     'YLim',[0 1],'YTick',[0 .5 1],'FontSize',14,'FontWeight','Bold');
 ylabel(ax2,'\betaM')
@@ -42,19 +42,21 @@ txt2=text(ax2,0.7,1.3,'eP_l_A = \betaM . eA_B','FontSize',14,'FontWeight','bold'
 
 
 hold(ax3)
-plot(ax3,TStroke.FM,TStroke.BM,'ok','MarkerFaceColor',[0.9 0.5 0.9])
-xdata=[TStroke.FM];
-ydata=[TStroke.BM];
-ll=findobj(ax3,'Type','Line');
-[r,m,b] = regression(xdata,ydata,'one');
-r=num2str(round(r,2));
-rfit=b+xdata.*m;
-plot(ax3,xdata,rfit,'LineWidth',2,'Color',[0.5 0.5 0.5])
-legend(ax3,ll,{'STROKE'},'box','off', 'Position',[0.506 0.38 0.42 0.08])
-set(ax3,'XLim',[20 35],'XTick',[20 25 30 35],'YLim',[0 1],'YTick',[0 0.5 1],'FontSize',14,'FontWeight','Bold');
-ylabel(ax3,'\betaM')
-xlabel(ax3,'FUGL-MEYER')
-tx=text(ax3,-0.3, 18,['r=',r,',p=0.75']);set(tx,'FontSize',14)
+bar(ax3,[1,4],[nanmean(TControl.eAMagn) nanmean(TControl.ePMagn)],'BarWidth',0.3,'FaceColor',[0.4 0.7 0.7])
+
+% plot(ax3,TStroke.FM,TStroke.BM,'ok','MarkerFaceColor',[0.9 0.5 0.9])
+% xdata=[TStroke.FM];
+% ydata=[TStroke.BM];
+% ll=findobj(ax3,'Type','Line');
+% [r,m,b] = regression(xdata,ydata,'one');
+% r=num2str(round(r,2));
+% rfit=b+xdata.*m;
+% plot(ax3,xdata,rfit,'LineWidth',2,'Color',[0.5 0.5 0.5])
+% legend(ax3,ll,{'STROKE'},'box','off', 'Position',[0.506 0.38 0.42 0.08])
+% set(ax3,'XLim',[20 35],'XTick',[20 25 30 35],'YLim',[0 1],'YTick',[0 0.5 1],'FontSize',14,'FontWeight','Bold');
+% ylabel(ax3,'\betaM')
+% xlabel(ax3,'FUGL-MEYER')
+% tx=text(ax3,-0.3, 18,['r=',r,',p=0.75']);set(tx,'FontSize',14)
 
 
 
