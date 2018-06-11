9dd58d60e594099a0dfe162097c8f09f8d121afd
dignadekam <digna.dekam@gmail.com>
2018-06-11 17:30:36 -0400

    changes to figs and regressions

--- ModulationMuscleActStroke/SpeedCorrectedGroupComparison.m
+++ ModulationMuscleActStroke/SpeedCorrectedGroupComparison.m
@@ -8,8 +8,12 @@ loadName=[matDataDir,loadName];
 load(loadName)
 
 AddCombinedParamsToTable;
+Idx=[1:19 21:30]';
+
 
 t.group=nominal(t.group);
+t2=t(Idx,:);
+
 
 logfile='VelocityCorrectedBetweenGroup';
 diary(logfile);
@@ -36,34 +40,35 @@ lm=fitlm(t,'eP_lA_netContributionPNorm~group + vel')
 lm=fitlm(t,'eP_B_netContributionPNorm~group + vel')
 
 disp('SINGLE LEG PARAMETERS')
-lm=fitlm(t,'eA_B_alphaSlow~group + vel')
-lm=fitlm(t,'lA_B_alphaSlow~group + vel')
-lm=fitlm(t,'eP_lA_alphaSlow~group + vel')
-lm=fitlm(t,'eP_B_alphaSlow~group + vel')
-
-lm=fitlm(t,'eA_B_alphaFast~group + vel')
-lm=fitlm(t,'lA_B_alphaFast~group + vel')
-lm=fitlm(t,'eP_lA_alphaFast~group + vel')
-lm=fitlm(t,'eP_B_alphaFast~group + vel')
-
-lm=fitlm(t,'eA_B_xSlow~group + vel')
-lm=fitlm(t,'lA_B_xSlow~group + vel')
-lm=fitlm(t,'eP_lA_xSlow~group + vel')
-lm=fitlm(t,'eP_B_xSlow~group + vel')
-
-lm=fitlm(t,'eA_B_xFast~group + vel')
-lm=fitlm(t,'lA_B_xFast~group + vel')
-lm=fitlm(t,'eP_lA_xFast~group + vel')
-lm=fitlm(t,'eP_B_xFast~group + vel')
+% lm=fitlm(t,'eA_B_alphaSlow~group + vel')
+% lm=fitlm(t,'lA_B_alphaSlow~group + vel')
+% lm=fitlm(t,'eP_lA_alphaSlow~group + vel')
+% lm=fitlm(t,'eP_B_alphaSlow~group + vel')
+% 
+% lm=fitlm(t,'eA_B_alphaFast~group + vel')
+% lm=fitlm(t,'lA_B_alphaFast~group + vel')
+% lm=fitlm(t,'eP_lA_alphaFast~group + vel')
+% lm=fitlm(t,'eP_B_alphaFast~group + vel')
+% 
+% lm=fitlm(t,'eA_B_xSlow~group + vel')
+% lm=fitlm(t,'lA_B_xSlow~group + vel')
+% lm=fitlm(t,'eP_lA_xSlow~group + vel')
+% lm=fitlm(t,'eP_B_xSlow~group + vel')
+% 
+% lm=fitlm(t,'eA_B_xFast~group + vel')
+% lm=fitlm(t,'lA_B_xFast~group + vel')
+% lm=fitlm(t,'eP_lA_xFast~group + vel')
+% lm=fitlm(t,'eP_B_xFast~group + vel')
 
 lm=fitlm(t,'FF_skneeAngleAtSHS~group + vel')
 lm=fitlm(t,'FB_skneeAngleAtSHS~group + vel')
 
 disp('EMG PARAMETERS')
 
-lm=fitlm(t,'BM~group + vel')
+lm=fitlm(t2,'BM~group + vel')
 lm=fitlm(t,'eAMagn~group + vel')
-lm=fitlm(t,'ePMagn~group + vel')
+lm=fitlm(t2,'ePMagn~group + vel')
+lm=fitlm(t2,'ePBMagn~group +vel')
 lm=fitlm(t,'FF_Quad~group + vel')
 lm=fitlm(t,'FB_Quad~group + vel')
 
