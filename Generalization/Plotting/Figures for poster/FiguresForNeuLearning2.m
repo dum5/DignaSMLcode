21e217fb76ca89434eaa4b720e2c5593eefc5968
dignadekam <digna.dekam@gmail.com>
2018-06-29 17:16:08 -0400

    poster figures final

--- Generalization/Plotting/Figures for poster/FiguresForNeuLearning2.m
+++ Generalization/Plotting/Figures for poster/FiguresForNeuLearning2.m
@@ -61,13 +61,13 @@ ylabel(ax2,'After Effect Overground','FontWeight','Bold','FontSize',20)
 ll=flipud(findobj(ax2,'Type','Bar'));
 legend(ax2,ll,{'Control Large Error','Explicit Small Error','Implicit Small Error'},'Position',[0.75 0.80 0.17 0.16],'box','off')
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Experiment 2: control, fullabrupt, catch %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Experiment 2: control, catch %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %initialize figure
 f2=figure('Name','Experiment 2');
-set(f2,'Color',[1 1 1]','Units','inches','Position',[0 0 12 5]);
+set(f2,'Color',[1 1 1]','Units','inches','Position',[0 0 10 5]);
 ax1 = axes('Position',[0.1   0.15   0.45 0.73],'FontSize',16);
 ax2 = axes('Position',[0.65  0.15   0.32 0.73],'FontSize',16);
 
@@ -76,25 +76,25 @@ hold(ax1)
 bar(ax1,1,nanmean(TControl.maxError),'FaceColor',[0.2 0.2 1],'BarWidth',0.7);
 errorbar(ax1,1,nanmean(TControl.maxError),nanstd(TControl.maxError)./sqrt(length(TControl.maxError)),...
     'Color','k','LineWidth',2)
-bar(ax1,2,nanmean(TFullAbrupt.maxError),'FaceColor',[0.8 0 0],'BarWidth',0.7);
-errorbar(ax1,2,nanmean(TFullAbrupt.maxError),nanstd(TFullAbrupt.maxError)./sqrt(length(TFullAbrupt.maxError)),...
+% bar(ax1,2,nanmean(TFullAbrupt.maxError),'FaceColor',[0.8 0 0],'BarWidth',0.7);
+% errorbar(ax1,2,nanmean(TFullAbrupt.maxError),nanstd(TFullAbrupt.maxError)./sqrt(length(TFullAbrupt.maxError)),...
+%     'Color','k','LineWidth',2)
+bar(ax1,2,nanmean(TCatch.maxError),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
+errorbar(ax1,2,nanmean(TCatch.maxError),nanstd(TCatch.maxError)./sqrt(length(TCatch.maxError)),...
     'Color','k','LineWidth',2)
-bar(ax1,3,nanmean(TCatch.maxError),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
-errorbar(ax1,3,nanmean(TCatch.maxError),nanstd(TCatch.maxError)./sqrt(length(TCatch.maxError)),...
+bar(ax1,3,nanmean(TCatch.netContributionNorm2_catch),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
+errorbar(ax1,3,nanmean(TCatch.netContributionNorm2_catch),nanstd(TCatch.netContributionNorm2_catch)./sqrt(length(TCatch.netContributionNorm2_catch)),...
     'Color','k','LineWidth',2)
-bar(ax1,4,nanmean(TCatch.netContributionNorm2_catch),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
-errorbar(ax1,4,nanmean(TCatch.netContributionNorm2_catch),nanstd(TCatch.netContributionNorm2_catch)./sqrt(length(TCatch.netContributionNorm2_catch)),...
+bar(ax1,4,nanmean(TCatch.netContributionNorm2_resumeSplit),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
+errorbar(ax1,4,nanmean(TCatch.netContributionNorm2_resumeSplit),nanstd(TCatch.netContributionNorm2_resumeSplit)./sqrt(length(TCatch.netContributionNorm2_resumeSplit)),...
     'Color','k','LineWidth',2)
-bar(ax1,5,nanmean(TCatch.netContributionNorm2_resumeSplit),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
-errorbar(ax1,5,nanmean(TCatch.netContributionNorm2_resumeSplit),nanstd(TCatch.netContributionNorm2_resumeSplit)./sqrt(length(TCatch.netContributionNorm2_resumeSplit)),...
-    'Color','k','LineWidth',2)
-plot(ax1,[3.5 3.5],[-0.4 0.4],'--k','LineWidth',2,'Color',[0.5 0.5 0.5])
+plot(ax1,[2.5 2.5],[-0.4 0.4],'--k','LineWidth',2,'Color',[0.5 0.5 0.5])
 text(ax1,3.6,-0.05,'Catch Error','FontSize',14,'FontWeight','Bold')
 text(ax1,4.6,0.05,'After Catch','FontSize',14,'FontWeight','Bold')
 text(ax1,0.75,0.05,'Maximum Error Adaptation','FontSize',14,'FontWeight','Bold')
 
-plot(ax1,[1 2],[-0.35 -0.35],'Color','k','LineWidth',2)
-set(ax1,'XLim',[0.5 5.5],'XTickLabel',{''},'YTick',[-0.4 -0.2 0 0.2 0.4])
+%plot(ax1,[1 2],[-0.35 -0.35],'Color','k','LineWidth',2)
+set(ax1,'XLim',[0.5 4.5],'XTickLabel',{''},'YTick',[-0.4 -0.2 0 0.2 0.4])
 ylabel(ax1,'Error Adaptation','FontWeight','Bold','FontSize',20)
 
 %plot OG after effects
@@ -102,17 +102,17 @@ hold(ax2)
 bar(ax2,1,nanmean(TControl.netContributionNorm2_OG_P),'FaceColor',[0.2 0.2 1],'BarWidth',0.7);
 errorbar(ax2,1,nanmean(TControl.netContributionNorm2_OG_P),nanstd(TControl.netContributionNorm2_OG_P)./sqrt(length(TControl.netContributionNorm2_OG_P)),...
     'Color','k','LineWidth',2)
-bar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),'FaceColor',[0.8 0 0],'BarWidth',0.7);
-errorbar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),nanstd(TFullAbrupt.netContributionNorm2_OG_P)./sqrt(length(TFullAbrupt.netContributionNorm2_OG_P)),...
+% bar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),'FaceColor',[0.8 0 0],'BarWidth',0.7);
+% errorbar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),nanstd(TFullAbrupt.netContributionNorm2_OG_P)./sqrt(length(TFullAbrupt.netContributionNorm2_OG_P)),...
+%     'Color','k','LineWidth',2)
+bar(ax2,2,nanmean(TCatch.netContributionNorm2_OG_P),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
+errorbar(ax2,2,nanmean(TCatch.netContributionNorm2_OG_P),nanstd(TCatch.netContributionNorm2_OG_P)./sqrt(length(TCatch.netContributionNorm2_OG_P)),...
     'Color','k','LineWidth',2)
-bar(ax2,3,nanmean(TCatch.netContributionNorm2_OG_P),'FaceColor',[0.67 0.85 0.30],'BarWidth',0.7);
-errorbar(ax2,3,nanmean(TCatch.netContributionNorm2_OG_P),nanstd(TCatch.netContributionNorm2_OG_P)./sqrt(length(TCatch.netContributionNorm2_OG_P)),...
-    'Color','k','LineWidth',2)
-plot(ax2,[1 3],[0.125 0.125],'Color','k','LineWidth',2)
-set(ax2,'XLim',[0.5 3.5],'XTickLabel',{''},'YLim',[0 0.15],'YTick',[0 0.05 0.1 0.15])
+plot(ax2,[1 2],[0.125 0.125],'Color','k','LineWidth',2)
+set(ax2,'XLim',[0.5 2.5],'XTickLabel',{''},'YLim',[0 0.15],'YTick',[0 0.05 0.1 0.15])
 ylabel(ax2,'After Effect Overground','FontWeight','Bold','FontSize',20)
 ll=flipud(findobj(ax2,'Type','Bar'));
-legend(ax2,ll,{'Control','Abrupt Perturbation','Catch'},'Position',[0.80 0.80 0.17 0.16],'box','off')
+legend(ax2,ll,{'Control','Catch'},'Position',[0.80 0.80 0.17 0.16],'box','off')
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -121,35 +121,58 @@ legend(ax2,ll,{'Control','Abrupt Perturbation','Catch'},'Position',[0.80 0.80 0.
 
 %initialize figure
 f3=figure('Name','Experiment 3');
-set(f3,'Color',[1 1 1]','Units','inches','Position',[0 0 8 5])
+set(f3,'Color',[1 1 1]','Units','inches','Position',[0 0 10 5])
 ax1 = axes('Position',[0.1   0.15   0.35 0.73],'FontSize',16);
 ax2 = axes('Position',[0.60   0.15   0.35 0.73],'FontSize',16);
 
 %plot error size
 hold(ax1)
-bar(ax1,1,nanmean(TTMAbruptNoFeedback.maxError),'FaceColor',[0.1 0.5 0.8],'BarWidth',0.7);
-errorbar(ax1,1,nanmean(TTMAbruptNoFeedback.maxError),nanstd(TTMAbruptNoFeedback.maxError)./sqrt(length(TTMAbruptNoFeedback.maxError)),...
+bar(ax1,1,nanmean(TControl.maxError),'FaceColor',[0.2 0.2 1],'BarWidth',0.7);
+errorbar(ax1,1,nanmean(TControl.maxError),nanstd(TControl.maxError)./sqrt(length(TControl.maxError)),...
     'Color','k','LineWidth',2)
-bar(ax1,2,nanmean(TTMFullAbrupt.maxError),'FaceColor',[0.8 0.35 0.35],'BarWidth',0.7);
-errorbar(ax1,2,nanmean(TTMFullAbrupt.maxError),nanstd(TTMFullAbrupt.maxError)./sqrt(length(TTMFullAbrupt.maxError)),...
+
+bar(ax1,2,nanmean(TFullAbrupt.maxError),'FaceColor',[0.8 0 0],'BarWidth',0.7);
+errorbar(ax1,2,nanmean(TFullAbrupt.maxError),nanstd(TFullAbrupt.maxError)./sqrt(length(TFullAbrupt.maxError)),...
     'Color','k','LineWidth',2)
-plot(ax1,[1 2],[-0.29 -0.29],'Color','k','LineWidth',2)
-set(ax1,'XLim',[0.5 2.5],'XTickLabel',{''},'YTick',[-0.3 -0.2 -0.1 0])
+
+bar(ax1,3,nanmean(TTMAbruptNoFeedback.maxError),'EdgeColor',[0.2 0.2 1],'LineWidth',2,'FaceColor',[1 1 1],'BarWidth',0.7);
+errorbar(ax1,3,nanmean(TTMAbruptNoFeedback.maxError),nanstd(TTMAbruptNoFeedback.maxError)./sqrt(length(TTMAbruptNoFeedback.maxError)),...
+    'Color','k','LineWidth',2)
+
+bar(ax1,4,nanmean(TTMFullAbrupt.maxError),'EdgeColor',[0.8 0 0],'LineWidth',2,'FaceColor',[1 1 1],'BarWidth',0.7);
+errorbar(ax1,4,nanmean(TTMFullAbrupt.maxError),nanstd(TTMFullAbrupt.maxError)./sqrt(length(TTMFullAbrupt.maxError)),...
+    'Color','k','LineWidth',2)
+
+
+set(ax1,'XLim',[0.5 4.5],'XTickLabel',{''},'YTick',[-0.3 -0.2 -0.1 0])
 ylabel(ax1,'Maximum Error Adaptation','FontWeight','Bold','FontSize',20)
+
+plot(ax1,[1 2],[-0.32 -0.32],'Color','k','LineWidth',2)
+plot(ax1,[3 4],[-0.32 -0.32],'Color','k','LineWidth',2)
+
 
 %plot TM after effects
 hold(ax2)
-bar(ax2,1,nanmean(TTMAbruptNoFeedback.netContributionNorm2_TM_P),'FaceColor',[0.1 0.5 0.8],'BarWidth',0.7);
-errorbar(ax2,1,nanmean(TTMAbruptNoFeedback.netContributionNorm2_TM_P),nanstd(TTMAbruptNoFeedback.netContributionNorm2_TM_P)./sqrt(length(TTMAbruptNoFeedback.netContributionNorm2_TM_P)),...
+bar(ax2,1,nanmean(TControl.netContributionNorm2_OG_P),'FaceColor',[0.2 0.2 1],'BarWidth',0.7);
+errorbar(ax2,1,nanmean(TControl.netContributionNorm2_OG_P),nanstd(TControl.netContributionNorm2_OG_P)./sqrt(length(TControl.netContributionNorm2_OG_P)),...
     'Color','k','LineWidth',2)
-bar(ax2,2,nanmean(TTMFullAbrupt.netContributionNorm2_TM_P),'FaceColor',[0.8 0.35 0.35],'BarWidth',0.7);
-errorbar(ax2,2,nanmean(TTMFullAbrupt.netContributionNorm2_TM_P),nanstd(TTMFullAbrupt.netContributionNorm2_TM_P)./sqrt(length(TTMFullAbrupt.netContributionNorm2_TM_P)),...
-    'Color','k','LineWidth',2)
-set(ax2,'XLim',[0.5 2.5],'XTickLabel',{''},'YLim',[0 0.30],'YTick',[0 0.1 0.2 0.3])
-ylabel(ax2,'After Effect Treadmill','FontWeight','Bold','FontSize',20)
-ll=flipud(findobj(ax2,'Type','Bar'));
-legend(ax2,ll,{'Control','Abrupt Perturbation'},'Position',[0.75 0.80 0.17 0.16],'box','off')
 
+bar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),'FaceColor',[0.8 0 0],'BarWidth',0.7);
+errorbar(ax2,2,nanmean(TFullAbrupt.netContributionNorm2_OG_P),nanstd(TFullAbrupt.netContributionNorm2_OG_P)./sqrt(length(TFullAbrupt.netContributionNorm2_OG_P)),...
+    'Color','k','LineWidth',2)
+
+bar(ax2,3,nanmean(TTMAbruptNoFeedback.netContributionNorm2_TM_P),'EdgeColor',[0.2 0.2 1],'LineWidth',2,'FaceColor',[1 1 1],'BarWidth',0.7);
+errorbar(ax2,3,nanmean(TTMAbruptNoFeedback.netContributionNorm2_TM_P),nanstd(TTMAbruptNoFeedback.netContributionNorm2_TM_P)./sqrt(length(TTMAbruptNoFeedback.netContributionNorm2_TM_P)),...
+    'Color','k','LineWidth',2)
+
+bar(ax2,4,nanmean(TTMFullAbrupt.netContributionNorm2_TM_P),'EdgeColor',[0.8 0 0],'LineWidth',2,'FaceColor',[1 1 1],'BarWidth',0.7);
+errorbar(ax2,4,nanmean(TTMFullAbrupt.netContributionNorm2_TM_P),nanstd(TTMFullAbrupt.netContributionNorm2_TM_P)./sqrt(length(TTMFullAbrupt.netContributionNorm2_TM_P)),...
+    'Color','k','LineWidth',2)
+ll=flipud(findobj(ax2,'Type','Bar'));
+legend(ax2,ll,{'Control (OG post)','Abrupt Pert (OM post)','Control (TM post)','Abrupt pert (TM post)'},'Position',[0.75 0.80 0.17 0.16],'box','off')
+
+set(ax2,'XLim',[0.5 4.5],'XTickLabel',{''},'YTick',[0 0.1 0.2 0.3])
+ylabel(ax2,'After Effect','FontWeight','Bold','FontSize',20)
 %initialize figure
 lower=0.07;
 height=0.09;
