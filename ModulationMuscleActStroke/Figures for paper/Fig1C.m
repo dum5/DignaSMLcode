8006c03f2f9f64f13846c2d8768edf7f68facc41
dignadekam <digna.dekam@gmail.com>
2018-07-31 12:21:10 -0400

    Figs stroke paper

--- ModulationMuscleActStroke/Figures for paper/Fig1C.m
+++ ModulationMuscleActStroke/Figures for paper/Fig1C.m
@@ -1,143 +1,116 @@
-% clear all
-% close all
-% %% Load data
-% subj='C0014';
-% load(['Z:\SubjectData\E01 Synergies\mat\HPF30\' subj '.mat']);
+%clear all
+close all
+%% Load data
+subj='C0014';
+%load(['Z:\SubjectData\E01 Synergies\mat\HPF30\' subj '.mat']);
+
 
 %% Align it
 conds={'TM Base','Adap'};
 events={'RHS','LTO','LHS','RTO'};
-alignmentLengths=[16,64,16,64];
-muscle='LG';
+alignmentLengths=[16,32,16,32];
+muscle='MG';
 RBase=expData.getAlignedField('procEMGData',conds(1),events,alignmentLengths).getPartialDataAsATS({['R' muscle]});
 LBase=expData.getAlignedField('procEMGData',conds(1),events([3,4,1,2]),alignmentLengths).getPartialDataAsATS({['L' muscle]});
 RAdap=expData.getAlignedField('procEMGData',conds(2),events,alignmentLengths).getPartialDataAsATS({['R' muscle]});
 LAdap=expData.getAlignedField('procEMGData',conds(2),events([3,4,1,2]),alignmentLengths).getPartialDataAsATS({['L' muscle]});
-
+figuresColorMap;
 %% Create plots
-fh=figure('Units','Normalized');
-set(gcf,'Color',[1 1 1])
-figuresColorMap
+for l=1:2
+    switch l
+        case 1
+            B=RBase;
+            A=RAdap;
+            tit=['F' muscle];
+            ycol=[0,0.447,0.741];
+        case 2
+            B=LBase;
+            A=LAdap;
+            tit=['S' muscle];
+            ycol=[0.85,0.325,0.098];
+    end
+fh=figure('Units','Normalized','Position',[0 0 .45 .2]);
 ph=[];
 ph1=[];
-prc=[16,84];%upper and lower percentile bounds of shaded area's
+prc=[16,84];
 MM=sum(alignmentLengths);
 M=cumsum([0 alignmentLengths]);
 xt=sort([M,M(1:end-1)+[diff(M)/2]]);
-xt=[0,8,16:16:80,88,96:16:160];
+phaseSize=8;
+xt=[0:phaseSize:MM];
 %xt=[0:8:MM];
 fs=16; %FontSize
-for i=1:2
-    ph(i)=axes();
-    set(ph(i),'Position',[.05+(i-1)*.33 .4 .25 .5]);
-    cc1=get(gca,'ColorOrder');
-    set(gca,'ColorOrder',condColors)
-    cca=get(gca,'ColorOrder');
-end
-linkaxes(ph,'y') 
 
-%baseline only
-i=1;
-B=LBase;
-B.plot(fh,ph(i),cca(1,:),[],0,[-49:0],prc,true);
-axis tight
-ylabel('')
-ylabel(['SLOW ' muscle])
-ax=gca;
-ax.YLabel.Color=cca(i,:);
-ax.YLabel.FontWeight='bold';
-
-ph1(i)=axes;
-set(ph1(i),'Position',[.05+(i-1)*.33 .15 .25 .1]);
-da=randn(1,12);
-aux=nanmedian(B.Data,3)';
-clear aux2
-for j=1:length(xt)-1
-    aux2(j)=mean(aux(xt(j)+1:xt(j+1)));%compute averages for checkerboards
-end
-aux3([1,2:2:10,11,12:2:20])=aux2;
-aux3([3:2:10,13:2:20])=aux2([3:6,9:12]);
-imagesc(aux3/max(nanmedian(B.Data,3)))
-view(2)
-colormap(flipud(map))
-caxis([-.5 .5])
-rectangle('Position',[.5 .5 20 1],'EdgeColor','k')
-set(ph1(i),'XTickLabel','','YTickLabel','','XTick','','YTick','')
-if i==2
-    pos=get(ph1(i),'Position');
-end
-
-for i=1:2
+    ph(l)=axes();
+    set(ph(l),'Position',[.07 .48 .35 .45]);
     hold on
-    switch i
-        case 1
-            B=LBase;
-            A=LAdap;
-            tit=['SLOW ' muscle];
-        case 2
-            B=RBase;
-            A=RAdap;
-            tit=['FAST ' muscle];
-     end
-    B.plot(fh,ph(i),cca(1,:),[],0,[-49:0],prc,true);
-    A.plot(fh,ph(i),cca(2,:),[],0,[-49:0],prc,true);
+
+    B.plot(fh,ph(l),condColors(1,:),[],0,[-49:0],prc,true);
+    A.plot(fh,ph(l),condColors(2,:),[],0,[-49:0],prc,true);
     axis tight
-    ylabel('')
-    ylabel(tit)
-    ax=gca;
-    ax.YLabel.Color=cc1(i,:);
-    ax.YLabel.FontWeight='bold';
+    ylabel('EMG','Color',ycol,'FontWeight','bold')
+    %ylabel(tit)
     
-    ph1(i)=axes;
-    set(ph1(i),'Position',[.05+(i-1)*.33 .15 .25 .1]);  
-    da=randn(1,12);
-    aux=nanmedian(A.Data,3)'-nanmedian(B.Data,3)';
-    clear aux2
-    for j=1:length(xt)-1
-        aux2(j)=mean(aux(xt(j)+1:xt(j+1)));%compute averages for checkerboards
+
+    %Add rectangles quantifying activity
+    for j=1:3
+        ph1(j)=axes;
+        set(ph1(j),'Position',[.07 .25+(j-1)*-.11 .35 .09]);  
+        drawnow
+        pause(1)
+        da=randn(1,12);
+        gamma=.5;
+        ex1=condColors(j,:);
+        map=niceMap(ex1,gamma);
+        switch j
+            case 1
+            aux=nanmedian(B.Data,3)';
+            tt='B';
+            case 2
+            aux=nanmedian(A.Data,3)';
+            tt='lA';
+            case 3
+            aux=1*(nanmedian(A.Data,3)'-nanmedian(B.Data,3)') +.5*max(nanmedian(B.Data,3));
+            figuresColorMap;
+            tt='lA_B';
+        end
+        clear aux2
+        for k=1:length(xt)-1
+            aux2(k)=mean(aux(xt(k)+1:xt(k+1)));
+        end
+        I=image(size(map,1)*aux2/max(nanmedian(B.Data,3)));
+        I.Parent.Colormap=flipud(map);
+        rectangle('Position',[.5 .5 12 1],'EdgeColor','k')
+        set(ph1(j),'XTickLabel','','YTickLabel','','XTick','','YTick','')
+        text(-.4-.1*(j-1)^2.6,1,tt,'Clipping','off','FontSize',14,'FontWeight','bold')
     end
-    aux3([1,2:2:10,11,12:2:20])=aux2;
-    aux3([3:2:10,13:2:20])=aux2([3:6,9:12]);
-    imagesc(aux3/max(nanmedian(B.Data,3)))
-    view(2)
-    colormap(flipud(map))
-    caxis([-.5 .5])
-    rectangle('Position',[.5 .5 20 1],'EdgeColor','k')
-    set(ph1(i),'XTickLabel','','YTickLabel','','XTick','','YTick','')
-    if i==2
-    pos=get(ph1(i),'Position');
-    end
-end
-drawnow
-for i=1:2
-    axes(ph(i))
-    ll=findobj(ph(i),'Type','Line');
+    drawnow
+    %
+
+
+    axes(ph(l))
+    ll=findobj(ph(l),'Type','Line');
     set(ll,'LineWidth',3)
-    set(ph(i),'FontSize',fs,'YTickLabel','','XTickLabel','','XTick',xt,'YTick','')
+    set(ph(l),'FontSize',fs,'YTickLabel','','XTickLabel','','XTick',xt,'YTick','')
     a=axis;
     yOff=a(3)-.2*(a(4)-a(3));
-    text(.015*MM,yOff,'DS','Clipping','off','FontSize',fs)
-    %text(.18*MM,yOff,{'Early'; 'Stance'},'Clipping','off','FontSize',fs)
-    %text(.36*MM,yOff,{'Late'; 'Stance'},'Clipping','off','FontSize',fs)
-    text(.2*MM,yOff,{'STANCE'},'Clipping','off','FontSize',fs)
-    text(.515*MM,yOff,'DS','Clipping','off','FontSize',fs)
-    %text(.68*MM,yOff,{'Early'; 'Swing'},'Clipping','off','FontSize',fs)
-    %text(.86*MM,yOff,{'Late';'Swing'},'Clipping','off','FontSize',fs)
-    text(.7*MM,yOff,{'SWING'},'Clipping','off','FontSize',fs)
+    %Add labels:
+    text(.25*2*phaseSize,yOff,'DS','Clipping','off','FontSize',fs)
+    text(1.15*2*phaseSize,yOff,{'STANCE'},'Clipping','off','FontSize',fs)
+    text(3.225*2*phaseSize,yOff,'DS','Clipping','off','FontSize',fs)
+    text(4.25*2*phaseSize,yOff,{'SWING'},'Clipping','off','FontSize',fs)
     axis(a)
-        hold on
+    hold on
     yOff=a(3)-.05*(a(4)-a(3));
-    plot([.1 .9]*16,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
-    plot([1.1 4.9]*16,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
-    plot([5.1 5.9]*16,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
-    plot([6.1 9.9]*16,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
-     if i==2
-        
-        legend(ll(end:-1:1),{'Baseline','Adaptation'},'Box','off','Position',[0.8497    0.7091    0.1490    0.2523])
-    end
+    %Add lines:
+    plot([.1 .9]*2*phaseSize,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
+    plot([1.1 2.9]*2*phaseSize,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
+    plot([3.1 3.9]*2*phaseSize,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
+    plot([4.1 5.9]*2*phaseSize,[1 1]*yOff,'Color',0*ones(1,3),'LineWidth',4,'Clipping','off')
+    legend(ll(end:-1:end-1),{'Baseline','Adaptation'})
+    
+    set(fh,'Position',[0 0 .45*1.5 .2*1.5])
+
+    %saveFig(fh,'./',['Fig1B_' num2str(l)],1)
 end
-set(gcf,'Position',[0 0 .7 0.2])
 %%
-%destdir='Z:\Users\Digna\Papers\Modulation of Muscle Activity in Stroke\Matlab Figures\';
-%saveFig(fh,cd, 'Fig1C',1)
-%print('-painters','-dsvg','Fig1C')
--- ModulationMuscleActStroke/Figures for paper/Fig1D.m
+++ ModulationMuscleActStroke/Figures for paper/Fig1D.m
@@ -4,7 +4,7 @@ fh=figure('Units','Normalized','Position',[0 0 .55 .35]);
     
 auxF=[0;.35;-.3];
 auxS=[-.35;.35;.2];
-for k=1:2
+for k=[2,1]
     switch k
         case 1 %eA
             aux1=auxF;
@@ -16,14 +16,16 @@ for k=1:2
             aux2=auxF;     
     end
 ax=axes;
-ax.Position=[0.05+(k-1)*.08 .05 .2 .35];
+%ax.Position=[0.05+(k-1)*.08 .05 .2 .35];
+ax.Position=[0.05+(k-1)*.16 .05 .2 .35];
 I=imshow(size(map,1)*(aux1+.5),flipud(map),'Border','tight');
 rectangle('Position',[.5 .5 1 3],'EdgeColor','k')
 %%Add arrows
 hold on
 quiver(ones(size(aux1)),[1:numel(aux1)]'+.4*sign(aux1),zeros(size(aux1)),-.7*sign(aux1),0,'Color','k','LineWidth',2)
 ax=axes;
-ax.Position=[.05+(k-1)*.08 .45 .2 .35];
+%ax.Position=[.05+(k-1)*.08 .45 .2 .35];
+ax.Position=[.05+(k-1)*.16 .45 .2 .35];
 I=imshow(size(map,1)*(aux2+.5),flipud(map),'Border','tight');
 rectangle('Position',[.5 .5 1 3],'EdgeColor','k')
 %%Add arrows
@@ -41,9 +43,9 @@ end
 
 %Add lines on fast/slow:
 ccc=get(gca,'ColorOrder');
-plot(-2*[1 1],3.45+[.5 3.5],'LineWidth',4,'Color',ccc(1,:),'Clipping','off')
-text(-2.5,6.5,'FAST','Color',ccc(1,:),'Rotation',90,'FontSize',20,'FontWeight','bold')
-plot(-2*[1 1],[.5 3.5],'LineWidth',4,'Color',ccc(2,:),'Clipping','off')
-text(-2.5,3,'SLOW','Color',ccc(2,:),'Rotation',90,'FontSize',20,'FontWeight','bold')
+plot(0*[1 1],3.45+[.5 3.5],'LineWidth',4,'Color',ccc(1,:),'Clipping','off')
+text(-0.6,6.5,'FAST','Color',ccc(1,:),'Rotation',90,'FontSize',20,'FontWeight','bold')
+plot(0*[1 1],[.5 3.5],'LineWidth',4,'Color',ccc(2,:),'Clipping','off')
+text(-0.6,3.25,'SLOW','Color',ccc(2,:),'Rotation',90,'FontSize',20,'FontWeight','bold')
 
 text(-3,-0.75,'\DeltaEMG(eP_l_A)=\beta_M \DeltaEMG(eA_B)^*','FontSize',16,'FontWeight','bold')
